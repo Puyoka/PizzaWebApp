86ad15043c9c20a68020eb72ad7cc23df866b2dc
Puyoka <nagysa1121@gmail.com>
2023-04-19 18:57:45 +0200

    WorkerOrdersUpdate

--- PizzaWebAppLibrary/DataAccess/IOrderData.cs
+++ PizzaWebAppLibrary/DataAccess/IOrderData.cs
@@ -8,5 +8,6 @@ public interface IOrderData
     Task<List<OrderModel>> GetCustomerCurrentOrders(string customerId);
     Task<List<OrderModel>> GetCustomerOrders(string customerId);
     Task<OrderModel> GetOrder(string orderId);
+    Task<List<IngredientModel>> GetPizzaIngredientsFromOrder(OrderModel order, int index);
     Task UpdateOrderStatus(OrderModel order);
 }
--- PizzaWebAppLibrary/DataAccess/MongoOrderData.cs
+++ PizzaWebAppLibrary/DataAccess/MongoOrderData.cs
@@ -4,12 +4,14 @@ public class MongoOrderData : IOrderData
 {
     private IMongoCollection<OrderModel> orders;
     private IMemoryCache cache;
+    private IDbConnection dbConn;
     private const string CacheName = "OrdersData";
 
     public MongoOrderData(IDbConnection _db, IMemoryCache _cache)
     {
         cache = _cache;
         orders = _db.OrderCollection;
+        dbConn = _db;
     }
 
     public Task CreateOrder(OrderModel order)
@@ -67,5 +69,45 @@ public class MongoOrderData : IOrderData
         var result = allOrders.Where(x => x.Id == orderId);
 
         return result.FirstOrDefault();
+    }
+
+    public async Task<List<IngredientModel>> GetPizzaIngredientsFromOrder(OrderModel order, int index)
+    {
+        var collectionName = dbConn.OrderCollectionName;
+        var db = dbConn.db;
+        var collection = db.GetCollection<BsonDocument>(collectionName);
+
+        var filter = Builders<BsonDocument>.Filter.Eq("_id", new ObjectId(order.Id));
+        var projection = Builders<BsonDocument>.Projection.Include("Products.Ingrediens");
+        var documents = collection.Find(filter).Project(projection).ToList();
+
+        var ingredients = new List<IngredientModel>();
+
+        var bsonArray = documents[0]["Products"].AsBsonArray;
+        if (bsonArray.Count - 1 >= index)
+        {
+            var bsonDocument = bsonArray[index].AsBsonDocument;
+            if (bsonDocument != null)
+            {
+                var bsonArrayIngredients = bsonDocument["Ingrediens"].AsBsonArray;
+
+                foreach (var bsonValueIngredient in bsonArrayIngredients)
+                {
+                    var bsonDocumentIngredient = bsonValueIngredient.AsBsonDocument;
+                    var ingredient = new IngredientModel
+                    {
+                        Id = bsonDocumentIngredient["_id"].ToString(),
+                        Name = bsonDocumentIngredient["Name"].ToString(),
+                        Price = bsonDocumentIngredient["Price"].ToInt32(),
+                        Allergens = null//bsonDocumentIngredient["Allergens"].AsBsonArray.Select(x => x.ToString()).ToList()
+                    };
+
+                    ingredients.Add(ingredient);
+
+                }
+            }
+
+        }
+        return ingredients;
     }
 }
